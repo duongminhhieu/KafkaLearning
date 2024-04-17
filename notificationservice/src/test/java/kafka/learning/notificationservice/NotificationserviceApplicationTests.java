b70f766cdae5b25b5c2e019b19ef6f0ae380390b
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-04-17 11:23:33 +0700

    create notificationservice

---
+++ notificationservice/src/test/java/kafka/learning/notificationservice/NotificationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kafka.learning.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
